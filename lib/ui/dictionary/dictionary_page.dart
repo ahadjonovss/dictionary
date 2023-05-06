a4633d9cec753d1a375e3383cf00e44edba7b830
ahadjonovss <samandarahadjonov@gmail.com>
2023-05-06 16:07:19 +0500

    project creating

--- lib/ui/dictionary/dictionary_page.dart
+++ lib/ui/dictionary/dictionary_page.dart
@@ -1,45 +1,65 @@
-
 import 'package:dictionary/utils/tools/file_importer.dart';
+import 'package:dictionary/utils/tools/sizes.dart';
 
 class DictionaryPage extends StatefulWidget {
+  const DictionaryPage({super.key});
+
   @override
-  _DictionaryPageState createState() => _DictionaryPageState();
+  DictionaryPageState createState() => DictionaryPageState();
 }
 
-class _DictionaryPageState extends State<DictionaryPage> {
-  TextEditingController _searchController = TextEditingController();
-  List<String> _wordList = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+class DictionaryPageState extends State<DictionaryPage> {
+  TextEditingController searchController = TextEditingController();
+  List<String> wordList = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dictionary App'),
-      ),
       body: Column(
         children: [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Search for a word',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: height(context)*0.18,
+                width: width(context),
+                color: Colors.blue,
+
+              ),
+              IconButton(onPressed: () {
+              }, icon: const Icon(Icons.menu,color: Colors.white,)),
+              Positioned(
+                bottom: -30,
+                left: 10,
+                right: 10,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  width: width(context)*0.9,
+                  padding:const  EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search for a word',
+                      prefixIcon:const  Icon(Icons.search),
+                      border: InputBorder.none
+                    ),
+                    onChanged: (value) {
+                      // TODO: Implement search functionality
+                    },
+                  ),
                 ),
               ),
-              onChanged: (value) {
-                // TODO: Implement search functionality
-              },
-            ),
+            ],
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: _wordList.length,
+              itemCount: wordList.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(_wordList[index]),
+                  title: Text(wordList[index]),
                   onTap: () {
                     // TODO: Implement word details page
                   },
---
+++ lib/utils/tools/sizes.dart
@@ -0,0 +1,4 @@
+import 'package:dictionary/utils/tools/file_importer.dart';
+
+double width(context)=>MediaQuery.of(context).size.width;
+double height(context)=>MediaQuery.of(context).size.height;
