29c89374d7f52e6fc88277467dba0057ea1c1315
ahadjonovss <samandarahadjonov@gmail.com>
2023-05-06 15:42:38 +0500

    project creating

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/material.dart';
+import 'package:dictionary/utils/tools/file_importer.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Image.asset('assets/1.png',width: 300,),
+            ]
+        ),
+      ),
+    );
   }
 }
