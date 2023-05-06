5c6b4e534e13d45ba910c4e456450e10114d2d11
ahadjonovss <samandarahadjonov@gmail.com>
2023-05-06 15:55:44 +0500

    project creating

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,7 +1,5 @@
-import 'package:dictionary/ui/home/home_page.dart';
 import 'package:dictionary/utils/tools/file_importer.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/route_manager.dart';
+
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -22,7 +20,7 @@ class _SplashPageState extends State<SplashPage> {
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            children: const [
               //Image.asset('assets/1.png',width: 300,),
               Text("Splash")
 
--- lib/utils/tools/file_importer.dart
+++ lib/utils/tools/file_importer.dart
@@ -2,3 +2,6 @@ export 'package:dictionary/ui/splash/splash_page.dart';
 export 'package:flutter/material.dart';
 export 'package:dictionary/app/app.dart';
 export 'package:get/get_navigation/src/root/get_material_app.dart';
+export 'package:get/get_core/src/get_main.dart';
+export 'package:get/route_manager.dart';
+export 'package:dictionary/ui/dictionary/dictionary_page.dart';
