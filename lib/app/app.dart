6bd50be5e27ce21b011f57c73780a881ecd1fc32
ahadjonovss <samandarahadjonov@gmail.com>
2023-05-06 15:41:10 +0500

    project creating

---
+++ lib/app/app.dart
@@ -0,0 +1,28 @@
+import 'package:dictionary/utils/tools/file_importer.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Yangi Konstitutsiya tahriri',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashPage()
+    );
+  }
+}
---
+++ lib/utils/tools/file_importer.dart
@@ -0,0 +1,3 @@
+export 'package:dictionary/ui/splash/splash_page.dart';
+export 'package:flutter/material.dart';
+export 'package:dictionary/app/app.dart';
