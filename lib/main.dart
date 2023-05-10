8939cd6fb2e53fd52ac0d9b7b7e1f505dd610f00
ahadjonovss <samandarahadjonov@gmail.com>
2023-05-10 10:25:35 +0500

    response done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:dictionary/utils/tools/file_importer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
   Get.put(ThemeController());
   Get.put(DictionaryController());
   runApp(const App());
--- lib/ui/dictionary/widgets/search_bar.dart
+++ lib/ui/dictionary/widgets/search_bar.dart
@@ -6,56 +6,54 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(20),
-          height: height(context)*0.18,
-          width: width(context),
-          alignment: Alignment.centerLeft,
-          color: Theme.of(context).accentColor,
-          child: IconButton(onPressed: () {
-            myKey.currentState!.openDrawer();
-          }, icon: const Icon(Icons.menu,color: Colors.white,)),
-
-
-        ),
-        Positioned(
-          bottom: -30,
-          left: 10,
-          right: 10,
-          child: Container(
-            decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3), // changes position of shadow
+    return Container(
+      padding: const EdgeInsets.all(12),
+      width: width(context),
+      alignment: Alignment.centerLeft,
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(onPressed: () {
+                myKey.currentState!.openDrawer();
+              }, icon: const Icon(Icons.menu,color: Colors.blue,)),
+              Container(
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                width: width(context)*0.8,
+                child: TextField(
+                  //controller: searchController,
+                  style: TextStyle(color: Get.theme.cardColor),
+                  decoration:  InputDecoration(
+                      hintText: 'Search for a word',
+                      prefixIcon:  Icon(Icons.search,color: Colors.black.withOpacity(0.5),),
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(color: Colors.black.withOpacity(0.5))
                   ),
-                ],
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12)
-            ),
-            width: width(context)*0.9,
-            padding:const  EdgeInsets.all(8.0),
-            child: TextField(
-              //controller: searchController,
-              style: TextStyle(color: Get.theme.cardColor),
-              decoration:  InputDecoration(
-                  hintText: 'Search for a word',
-                  prefixIcon:  Icon(Icons.search,color: Colors.black.withOpacity(0.5),),
-                  border: InputBorder.none,
-                hintStyle: TextStyle(color: Colors.black.withOpacity(0.5))
+                  onChanged: (value) {
+                    // TODO: Implement search functionality
+                  },
+                ),
               ),
-              onChanged: (value) {
-                // TODO: Implement search functionality
-              },
-            ),
-          ),
-        ),
-      ],
+
+            ],
+          )
+        ],
+      )
+
+
     );
   }
 }
